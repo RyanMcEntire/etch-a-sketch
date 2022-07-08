5c2d2d3f2d5ff489dc95292c26c66b053b4dcf81
RyanMcEntire <ryanmcentire89@gmail.com>
2022-07-08 13:30:26 -0600

    add write persistence

--- main.js
+++ main.js
@@ -1,26 +1,31 @@
 const gridContainer = document.querySelector(".grid-container");
-let x = 80;
-let y = 80;
+let x = 100;
+let y = 100;
 let grid = { x, y };
 const total = grid.x * grid.y;
 
 document.addEventListener("DOMContentLoaded", makeGrid);
 
 function makeGrid() {
+  //makes the boxes
   for (let i = 0; i < total; i++) {
-    const html = ``;
-    const divBox = gridMaker("div", gridContainer, html);
-    console.log("div", gridContainer, html)
+    const divBox = gridMaker("div", gridContainer);
   }
   gridContainer.style.setProperty(
     `grid-template-columns`,
     `repeat(${grid.x}, 1fr)`
   );
+  gridContainer.addEventListener('mouseover', write)
 }
 
-function gridMaker(eleType, parent, html,) {
+function gridMaker(boxType, parent) {
+  //
   const box = document.createElement("div");
-  box.innerHTML = html;
-  
-  return parent.appendChild(box).className = "grid-box";
+  box.classList.add("grid-box");
+
+  return parent.appendChild(box);
+}
+
+function write(e) {
+  e.target.classList.add("grid-box-draw");
 }
